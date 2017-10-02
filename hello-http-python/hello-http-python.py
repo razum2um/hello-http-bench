f48d5a16dc241b95453c0aa13b6dbc27bc9b7699
Alexander Shorin <kxepal@gmail.com>
2017-10-02 13:08:40 +0300

    Disable access log
    Access logging does quite work to slow down things in benchmarks.

--- hello-http-python/hello-http-python.py
+++ hello-http-python/hello-http-python.py
@@ -5,6 +5,7 @@ from aiohttp import web
 def create_app(argv=None):
     app = web.Application()
     app.router.add_get('/', hello_world)
+    app.make_handler(access_log=None)
     return app
 
 async def hello_world(request: web.Request) -> web.Response:
