afb44d4b736b8ca81ad738a2125e6ca80cecea20
Vlad Bokov <razum2um@mail.ru>
2017-10-03 02:42:29 +0700

    no input stream

--- hello-http-clojure/src/hello_http_bench/core.clj
+++ hello-http-clojure/src/hello_http_bench/core.clj
@@ -20,16 +20,15 @@
    :body (str prefix (System/currentTimeMillis))})
 
 ;; don't allocate full body string
-(def hello-input-stream (-> prefix .getBytes io/input-stream (doto (.mark Integer/MAX_VALUE))))
+(def hello-input (-> prefix .getBytes))
 (defrecord TimedResponse [time])
 (extend-protocol protocols/StreamableResponseBody
   TimedResponse
   (write-body-to-stream [body _ ^java.io.OutputStream output-stream]
     (with-open [out output-stream]
-      (io/copy hello-input-stream out)
-      (.write out (-> body :time str .getBytes)))
-    (.reset hello-input-stream)))
-(defn cache-input-stream [request]
+      (.write out hello-input)
+      (.write out (-> body :time str .getBytes)))))
+(defn handler [request]
   {:status 200
    :headers {}
    :body (->TimedResponse (System/currentTimeMillis))})
@@ -38,6 +37,6 @@
   [& args]
   ;; Cannot max=1: Insufficient threads: max=1 < needed(acceptors=1 + selectors=4 + request=1)
   (let [app (or (some-> args first (->> (str "hello-http-bench.core/")) symbol resolve)
-                cache-input-stream)]
+                handler)]
     (println "Listening" app "on 8288")
     (jetty/run-jetty app {:port 8288 :min-threads 1 :max-threads 6})))
