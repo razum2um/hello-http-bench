233a96e2c536cef041dd4bb75dfa3d4ada6800a4
Vlad Bokov <razum2um@mail.ru>
2017-10-02 22:41:22 +0700

    fix uwsgi impl

--- hello-http-python/hello-http-python.py
+++ hello-http-python/hello-http-python.py
@@ -2,4 +2,4 @@ import time
 
 def application(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return ["Hello world %d" % round(time.time() * 1000)]
+    return [("Hello world %d" % round(time.time() * 1000)).encode()]
